71a2b75db44ce9b8178fa11256d7279cbde501e3
iamknownasfesal <mkircal957@gmail.com>
2024-05-07 23:02:38 +0300

    tees

--- types/tees.ts
+++ types/tees.ts
@@ -1,21 +1,21 @@
 export type TeesContract = TeesEVMContract | TeesSUIContract;
 
 export type NFT = {
-    id: string;
-    image: string;
-    name: string;
-    collectionAddress: string;
-}
+  id: string;
+  image: string;
+  name: string;
+  collectionAddress: string;
+};
 
 type TeesEVMContract = {
-    address: string;
-    abi: unknown;
-    name: string;
-    type: "EVM";
-}
+  address: string;
+  abi: any;
+  name: string;
+  type: "EVM";
+};
 
 type TeesSUIContract = {
-    type: "SUI";
-    objectType: string;
-    name: string;
-}
+  type: "SUI";
+  objectType: string;
+  name: string;
+};
