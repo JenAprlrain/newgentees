b0d26c8e823a319fd01af767071c9fdc2c30d53d
iamknownasfesal <mkircal957@gmail.com>
2024-05-17 15:00:18 +0300

    please work ser

--- api/image.ts
+++ api/image.ts
@@ -4,12 +4,12 @@ import { getStorage, ref, uploadBytes } from "firebase/storage";
 import { createId } from "@paralleldrive/cuid2";
 
 const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_SENDER_ID,
-  appId: process.env.FB_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FB_APP_ID,
 };
 
 // Initialize Firebase
