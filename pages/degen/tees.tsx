39b3447111f3d284f51c8074a96028d31ddb1a7a
iamknownasfesal <mkircal957@gmail.com>
2024-05-17 13:02:29 +0300

    fix tees

--- pages/degen/tees.tsx
+++ pages/degen/tees.tsx
@@ -1,5 +1,18 @@
+import { getHouseCollections } from "@/api/house-collection";
+import { HouseCollectionType } from "@/types/house-collections";
 import { Tees } from "@/views/tees";
+import { useState, useEffect } from "react";
 
 export default function Page() {
-  return <Tees />;
+  const [HouseCollectionsData, setHouseCollectionsData] = useState<
+    HouseCollectionType[]
+  >([]);
+
+  useEffect(() => {
+    getHouseCollections()
+      .then((data) => data.json())
+      .then((data) => setHouseCollectionsData(data));
+  }, []);
+
+  return <Tees housecollections={HouseCollectionsData} />;
 }
