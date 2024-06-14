12a4e8a045f49e32e6c6edea1aba7bce30694c1b
JenAprlrain <jenheidt1@gmail.com>
2024-06-14 17:11:17 -0400

    swiched landing page and created home page for old landing page

---
+++ pages/general/home.tsx
@@ -0,0 +1,14 @@
+import { Link } from "@/components/link";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex flex-col gap-8 text-3xl">
+      <div>I&apos;M A:</div>
+      <Link href="/degen">DEGEN: LOOKING TO BUY/CLAIM NFT APPAREL.</Link>
+      <Link href="/collabs">DEV/ARTIST: LOOKING TO COLLABORATE.</Link>
+      {/* <Link href="/normie">NORMIE: JUST LET ME IN THE SITE.</Link> */}
+    </div>
+  );
+};
+
+export default Home;
--- views/landing/index.tsx
+++ views/landing/index.tsx
@@ -1,12 +1,65 @@
-import { Link } from "@/components/link";
+import React, { useEffect, useState } from 'react';
+import styles from '@/styles/LandingPage.module.css';
 
-export const Landing = () => {
+export const Landing: React.FC = () => {
+  const firstText = `Welcome TO THE FUTURE OF APPAREL`;
+  const [displayedText, setDisplayedText] = useState('');
+  const [index, setIndex] = useState(0);
+  const [showTees, setShowTees] = useState(false);
+  const [showEnterText, setShowEnterText] = useState(false);
+  const [showCursor, setShowCursor] = useState(true);
+
+  useEffect(() => {
+    const cursorInterval = setInterval(() => {
+      setShowCursor(prev => !prev);
+    }, 500); // Blinking cursor speed
+
+    return () => clearInterval(cursorInterval);
+  }, []);
+
+  useEffect(() => {
+    if (index < firstText.length) {
+      const timeoutId = setTimeout(() => {
+        setDisplayedText(displayedText + firstText.charAt(index));
+        setIndex(index + 1);
+      }, 100); // Adjust typing speed here
+
+      return () => clearTimeout(timeoutId);
+    } else {
+      const pauseBeforeTees = setTimeout(() => {
+        setShowTees(true);
+        setShowEnterText(true);
+      }, 1000); // Adjust delay here (1000ms = 1 second)
+
+      return () => clearTimeout(pauseBeforeTees);
+    }
+  }, [index, displayedText, firstText]);
+
+  const handleEnterClick = () => {
+    window.location.href = '/general/home'; // Change this to main website route
+  };
+
   return (
-    <div className="flex flex-col gap-8 text-3xl">
-      <div>I&apos;M A:</div>
-      <Link href="/degen">DEGEN: LOOKING TO BUY/CLAIM NFT APPAREL.</Link>
-      <Link href="/collabs">DEV/ARTIST: LOOKING TO COLLABORATE.</Link>
-      {/* <Link href="/normie">NORMIE: JUST LET ME IN THE SITE.</Link> */}
+    <div className={styles.container}>
+      <div className={styles.typingText}>
+        {displayedText}
+        {showCursor && <span className={styles.cursor}>_</span>}
+      </div>
+      {showTees && (
+        <div className={styles.animationContainer}>
+          <div className={styles.teesContainer}>
+            <div className={styles.teesText}>TEES</div>
+            <div className={styles.teesBorder}></div>
+          </div>
+        </div>
+      )}
+      {showEnterText && (
+        <div className={styles.enterTextContainer}>
+          <div className={styles.enterText} onClick={handleEnterClick}>
+            [ENTER]
+          </div>
+        </div>
+      )}
     </div>
   );
 };
